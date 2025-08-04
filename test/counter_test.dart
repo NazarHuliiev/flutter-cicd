cb36212a2a8ece6844f7f041aba1595b2e7c879d
Nazar Huliiev <nazar.huliev@gmail.com>
2025-08-04 21:44:36 +0300

    Add pull request workflow

---
+++ test/counter_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_cicd/counter_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Counter should be incremented', (){
+    final repository = CounterRepository();
+
+    repository.increment();
+
+    expect(repository.counter, 1);
+  });
+
+  test('Counter should be decremented', (){
+    final repository = CounterRepository();
+
+    repository.increment();
+    repository.decrement();
+
+    expect(repository.counter, 0);
+  });
+}
